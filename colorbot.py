f152d822a3c743baf5e33d41ce7cf85f398990e2
Andrew Tran <andrewwtran@berkeley.edu>
2021-06-29 15:55:39 -0500

    Test heroku deployment part 11

--- colorbot.py
+++ colorbot.py
@@ -76,7 +76,7 @@ def main():
     # Set schedule to post tweet every day at 12:00PM (PST)
     sched = BlockingScheduler({'apscheduler.timezone': 'US/Pacific'})
     sched.start
-    sched.add_job(lambda: post_tweet(path, hex_code), 'cron', hour=13, minute=10)
+    sched.add_job(lambda: post_tweet(path, hex_code), 'cron', hour=14, minute=0)
     try:
         # This is here to simulate application activity (which keeps the main thread alive).
         while True:
